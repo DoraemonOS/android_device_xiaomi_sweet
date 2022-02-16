bb3cfec5b7304b1e7db91a3669d81482685a12e5
dopaemon <polarisdp@gmail.com>
2022-02-16 10:45:49 +0000

    DoraemonOS
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2021 The PixelExperience Project
+# Copyright (C) 2021 The DoraemonOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_sweet.mk
+    $(LOCAL_DIR)/dora_sweet.mk
+
+COMMON_LUNCH_CHOICES := \
+    dora_sweet-eng \
+    dora_sweet-userdebug \
+    dora_sweet-user
--- dora_sweet.mk
+++ dora_sweet.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The PixelExperience Project
+# Copyright (C) 2021 The DoraemonOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,18 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common PixelExperience stuff
+# Inherit some common DoraemonOS stuff
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/dora/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_sweet
+PRODUCT_NAME := dora_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Maintainer Properties
+DORA_BUILD_TYPE := OFFICIAL
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.doraemon.maintainer=KernelPanix
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
